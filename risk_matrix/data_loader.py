d5709628bc64050b14b350e608eb3b61deac28e8
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-03-29 18:16:24 +0200

    def init

--- risk_matrix/data_loader.py
+++ risk_matrix/data_loader.py
@@ -7,3 +7,8 @@ ALPHA_VANTAGE_API_KEY = ""
 class MarketDataLoader:
     """Klasa do pobierania i wstępnego pobierania danych giełdowych"""
     BASE_URL = "https://www.alphavantage.co/query"
+
+    def __init__(self, tickers: list[str], api_key: str = ALPHA_VANTAGE_API_KEY):
+        self.tickers = tickers
+        self.api_key = api_key
+        self.data: pd.DataFrame | None = None
