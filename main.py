307f6db345b4cad4b7ab8b81fc98ff6d33874149
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-03-31 12:52:39 +0200

    print last 5 days of logarithmic returns in main

--- main.py
+++ main.py
@@ -8,4 +8,16 @@ def main():
 
     loader.fetch_data(start_date="2026-01-01", end_date="2026-03-31") #pobieranie danych
 
-    
+    print("\n statystyki portfela")
+
+    stats = loader.get_summary_stats()
+
+    print(stats)
+
+    returns = loader.calculate_log_returns() #logarytmiczne stopy zwrotu
+
+    print("\n ostatnie 5 dni zwrotów logarytmicznych:")
+    print(returns.tail())
+
+    if __name__=="__main__":
+        main()
