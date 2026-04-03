41456ade2ce18fd18cc6e45c2623c955ef520df0
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-04-03 19:52:10 +0200

    add risk engine in main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from risk_matrix.data_loader import MarketDataLoader
-
+from risk_matrix.stats_engine import RiskEngine
+import numpy as np
 def main():
     """portfel inwestycyjny"""
     assets = ['GOOGL', 'MSFT']
@@ -22,5 +23,20 @@ def main():
     print("\n ostatnie 5 dni zwrotów logarytmicznych:")
     print(returns.tail())
 
+    engine = RiskEngine(returns) #Inicjalizacja silnika ryzyka
+
+    #Obliczanie VaR dla pojedynczych akcji (przy portfelu 100 000 USD)
+    var_95 = engine.calculate_parametric_var(confidence_level=0.95, portfolio_value=100000)
+
+    print("\n 1-dniowy VaR (95% ufnosci) dla 100k USD inwestycji")
+    print(var_95)
+
+    #VaR całego portfela (równe wagi: po 25% na każde aktywo)
+    weights = np.array([0.25, 0.25, 0.25, 0.25])
+    
+    port_var = engine.calculate_portfolio_var(weights, confidence_level=0.95, portfolio_value=1000000)
+
+    print(f"\n całkowity Var portfela: {port_var:.2f} USD")
+    
 if __name__=="__main__":
     main()
