970ff2a55e0972a509c75e0c6819fbce91d7d97a
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-04-03 18:07:48 +0200

    add fetch data for rebuild yfinance

--- risk_matrix/data_loader.py
+++ risk_matrix/data_loader.py
@@ -1,85 +1,35 @@
 import pandas as pd
 import numpy as np
-import requests
-import os
-from dotenv import load_dotenv
+import yfinance as yf
 
-load_dotenv()
 
-api_key_os = os.getenv("ALPHA_VANTAGE_API_KEY")
 
 
 class MarketDataLoader:
     """Klasa do pobierania i wstępnego pobierania danych giełdowych"""
-    BASE_URL = "https://www.alphavantage.co/query"
+   
 
-    def __init__(self, tickers: list[str], api_key: str = api_key_os):
+    def __init__(self, tickers: list[str]):
         self.tickers = tickers
-        self.api_key = api_key
         self.data: pd.DataFrame | None = None
     
-    def fetch_single(self, ticker: str, start_date: str, end_date: str) -> pd.Series:
-        """Pobiera dane dla jednego tickera z Alpha Vantage"""
-        params = {
-            "function": "TIME_SERIES_DAILY",
-            "symbol": ticker,
-            "outputsize": "compact",
-            "apikey": self.api_key,
-        }
-        response = requests.get(self.BASE_URL, params=params)
-        response.raise_for_status()
-        json_data = response.json()
-        #print(f"z API przyszło: {ticker}: {json_data}")
-
-        if "Time Series (Daily)" not in json_data:
-
-            error_msg = (json_data.get("Note") or 
-            json_data.get("Information") or
-            json_data.get("Error Message") or 
-            "Nieznany blad API")
-
-            raise ValueError(f"blad dla {ticker}: {error_msg}")
-
-        series = json_data["Time Series (Daily)"]
-
-        adj_close = {
-            date: float(values["4. close"]) for date, values in series.items()
-        }
-        s = pd.Series(adj_close, name=ticker)
-        s.index = pd.to_datetime(s.index)
-        s = s.sort_index()
-
-        # Filtrujemy po zakresie dat
-        return s.loc[start_date:end_date]
-
     def fetch_data(self, start_date: str, end_date: str) -> pd.DataFrame:
-        """Pobiera skorygowane ceny zamknięcia z Alpha Vantage"""
-        print(f'pobieranie danych dla: {', '.join(self.tickers)}...')
-        frames: dict[str, pd.Series] = {}
+        """Pobiera ceny zamknięcia z yahoo finance"""
+        print(f"Pobieranie danych dla: {', '.join(self.tickers)}")
+
         try:
-            for ticker in self.tickers:
-                frames[ticker] = self.fetch_single(ticker, start_date, end_date)
-            self.data = pd.DataFrame(frames)
+            df = yf.download(self.tickers, start=start_date, end=end_date)
+            
+            #tylko ceny zamknięcia
+            if not df.empty:
+                self.data = df['Close']
+            else:
+                self.data = pd.DataFrame()
+            
             return self.data
+        
         except Exception as e:
-            print(f"błąd podzas pobierania danych: {e}")
+            print(f"błąd podczas pobierania danych: {e}")
             return pd.DataFrame()
-
-    def calculate_log_returns(self) -> pd.DataFrame:
-        """
-        oblicza logarytmiczne stopy zwrotu wedlug
-        r_t = ln(P_t / P_{t-1})
-        """
-        if self.data is None or self.data.empty:
-            raise ValueError("Brak danych. Najpierw wywołaj fetch_data().")
-    
-        log_returns = np.log(self.data / self.data.shift(1)).dropna()
-        return log_returns
-
-    def get_summary_stats(self) -> pd.DataFrame:
-        """Zwraca podstawowe statystyki opisowe dla pobranych aktywow"""
-        returns = self.calculate_log_returns()
-        summary = returns.aggregate(['mean', 'std', 'skew', 'kurtosis']).T
-        summary['annual_mean'] = summary['mean'] * 252
-        summary['annual_std'] = summary['std'] * np.sqrt(252)
-        return summary
+        
+        
