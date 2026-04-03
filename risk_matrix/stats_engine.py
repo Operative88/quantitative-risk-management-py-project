0e5e4e48f39d25e9ef3b10db5b7f37be11007ed9
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-04-03 19:18:58 +0200

    calculate parametric var

--- risk_matrix/stats_engine.py
+++ risk_matrix/stats_engine.py
@@ -12,3 +12,21 @@ class RiskEngine:
         """Oblicza macierz kowariancji zwrotów"""
         return self.returns.cov() * 252 #liczba dni handlowych w roku
     
+    def get_correlation_matrix(self) -> pd.DataFrame:
+        """Oblicza macierz korelacji między aktywami"""
+        return self.returns.corr()
+    
+    def calculate_parametric_var(self, confidence_level: float= 0.95, portfolio_value: float = 1000000) -> pd.Series:
+        """
+        Oblicza parametryczny value at risk dla każdego aktywa
+        zwraca wartosc w walucie (ile pln mozna stracic)
+        """
+        #srednia i odchylenie standardowe
+        mu = self.returns.mean()
+        sigma = self.returns.std()
+
+        #kwantyl rozkładu normalnego (z-score)
+        z_score = norm.ppf(1 - confidence_level)
+
+        var = abs(mu + z_score * sigma) * portfolio_value
+        return var
